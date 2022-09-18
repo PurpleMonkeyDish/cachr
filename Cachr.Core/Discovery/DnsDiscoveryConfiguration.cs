762d6ba6e64e14a648101dc3c7779744893669e3
Jason Couture <jasonc@alertr.info>
2022-09-18 15:55:51 -0400

    Add DNS based discovery

---
+++ Cachr.Core/Discovery/DnsDiscoveryConfiguration.cs
@@ -0,0 +1,7 @@
+namespace Cachr.Core.Discovery;
+
+public class DnsDiscoveryConfiguration
+{
+    public string HostName { get; set; } = string.Empty;
+    public int Port { get; set; } = 80;
+}
---
+++ Cachr.Core/Discovery/DnsPeerDiscoveryProvider.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.Extensions.Options;
+
+namespace Cachr.Core.Discovery;
+
+public class DnsPeerDiscoveryProvider : IPeerDiscoveryProvider, IDisposable
+{
+    private readonly IOptionsMonitor<DnsDiscoveryConfiguration> _options;
+    private DnsDiscoveryConfiguration _dnsDiscoveryConfiguration;
+    private readonly IDisposable _onChangeSubscription;
+
+    public DnsPeerDiscoveryProvider(IOptionsMonitor<DnsDiscoveryConfiguration> options)
+    {
+        _options = options;
+        _onChangeSubscription = _options.OnChange(OnConfigurationChanged);
+        _dnsDiscoveryConfiguration = options.CurrentValue;
+    }
+
+    private void OnConfigurationChanged(DnsDiscoveryConfiguration dnsDiscoveryConfiguration)
+    {
+        Interlocked.Exchange(ref _dnsDiscoveryConfiguration, dnsDiscoveryConfiguration);
+    }
+
+    public async Task<IEnumerable<string>> DiscoverPeersAsync(CancellationToken cancellationToken)
+    {
+        if(string.IsNullOrWhiteSpace(_dnsDiscoveryConfiguration.HostName))
+            return Enumerable.Empty<string>();
+        var hostEntries = await Dns.GetHostAddressesAsync(_dnsDiscoveryConfiguration.HostName);
+        if(hostEntries.Length == 0) return Enumerable.Empty<string>();
+        return EnumerateHostEntriesToUri(hostEntries);
+    }
+
+    private IEnumerable<string> EnumerateHostEntriesToUri(IPAddress[] hostEntries)
+    {
+        foreach (var address in hostEntries.Where(i =>
+                     i.AddressFamily == AddressFamily.InterNetwork
+                     ))
+        {
+            yield return $"{address}:{_dnsDiscoveryConfiguration.Port}";
+        }
+    }
+
+    public void Dispose()
+    {
+        _onChangeSubscription.Dispose();
+    }
+}
