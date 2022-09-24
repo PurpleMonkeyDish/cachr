de135ce40d79e9f107cdfd8dcfa8ae305c94ccd2
Jason Couture <jasonc@alertr.info>
2022-09-24 15:38:02 -0400

    Move classes to their own files.

--- Cachr.Core/Messaging/IMessageBus.cs
+++ Cachr.Core/Messaging/IMessageBus.cs
@@ -8,22 +8,3 @@ public interface IMessageBus<T>
     ISubscriptionToken Subscribe(ISubscriber<T> subscriber);
     void Unsubscribe(ISubscriptionToken subscriptionToken);
 }
-
-public static class MessageBusExtensions
-{
-    public static ISubscriptionToken Subscribe<T>(this IMessageBus<T> messageBus, Func<T, ValueTask> callback,
-        SubscriptionMode mode = SubscriptionMode.All)
-        where T : class
-    {
-        var subscription = new SubscriptionToken<T>(callback, messageBus, mode);
-        return messageBus.Subscribe(subscription);
-    }
-
-    public static ISubscriptionToken Subscribe<T>(this IMessageBus<T> messageBus, Func<T, object?, ValueTask> callback,
-        object? state = null, SubscriptionMode mode = SubscriptionMode.All)
-    where T : class
-    {
-        var subscription = new SubscriptionToken<T>(callback, messageBus, state, mode);
-        return messageBus.Subscribe(subscription);
-    }
-}
--- Cachr.Core/Messaging/MessageBus.cs
+++ Cachr.Core/Messaging/MessageBus.cs
@@ -5,15 +5,6 @@ using Microsoft.Extensions.Options;
 
 namespace Cachr.Core.Messaging;
 
-public interface ISubscriber<in T> : ISubscriptionToken
-    where T : class
-{
-    SubscriptionMode Mode { get; }
-    bool HandlesMode(SubscriptionMode mode) => Mode == SubscriptionMode.All || Mode.HasFlag(mode);
-    bool WillTryToHandleMessage(SubscriptionMode mode, T message) => true;
-    ValueTask<bool> OnMessageAsync(SubscriptionMode mode, T message);
-}
-
 public sealed class MessageBus<T> : IMessageBus<T>, IDisposable
     where T : class
 {
