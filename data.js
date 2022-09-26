c1170b256891ee4683705be483e671f43f949693
github-action-benchmark <github@users.noreply.github.com>
2022-09-26 00:07:34 +0000

    add Benchmark - MessageBusIterationBenchmarks - windows-latest (benchmarkdotnet) benchmark result for 26ee6f96cd80e030639dbd80ce1265f550c6b935

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1664150836660,
+  "lastUpdate": 1664150854948,
   "repoUrl": "https://github.com/jasoncouture/cachr",
   "entries": {
     "Benchmark - MessageBusIterationBenchmarks - ubuntu-latest": [
@@ -74,6 +74,44 @@ window.BENCHMARK_DATA = {
             "value": 751.566641031901,
             "unit": "ns",
             "range": "± 75.11259278509593"
+          }
+        ]
+      }
+    ],
+    "Benchmark - MessageBusIterationBenchmarks - windows-latest": [
+      {
+        "commit": {
+          "author": {
+            "email": "jasonc@alertr.info",
+            "name": "Jason Couture",
+            "username": "jasoncouture"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "26ee6f96cd80e030639dbd80ce1265f550c6b935",
+          "message": "Reduce the number of benchmarks being run (#31)",
+          "timestamp": "2022-09-25T19:59:06-04:00",
+          "tree_id": "e58cb37cb9c7ae6384cbdd17512ff8513605a2bc",
+          "url": "https://github.com/jasoncouture/cachr/commit/26ee6f96cd80e030639dbd80ce1265f550c6b935"
+        },
+        "date": 1664150854373,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Cachr.Benchmarks.MessageBusIterationBenchmarks.BroadcastAsyncBenchmark",
+            "value": 7279.6962890625,
+            "unit": "ns",
+            "range": "± 326.7177805042888"
+          },
+          {
+            "name": "Cachr.Benchmarks.MessageBusIterationBenchmarks.SendToAsyncBenchmark",
+            "value": 1022.8217976888021,
+            "unit": "ns",
+            "range": "± 18.37734716709683"
           }
         ]
       }
