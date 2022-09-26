d37549423f10e0331099d1504e80737735a6ef02
github-action-benchmark <github@users.noreply.github.com>
2022-09-26 00:06:51 +0000

    add Benchmark - MessageBusIterationBenchmarks - ubuntu-latest (benchmarkdotnet) benchmark result for 26ee6f96cd80e030639dbd80ce1265f550c6b935

---
+++ data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1664150811277,
+  "repoUrl": "https://github.com/jasoncouture/cachr",
+  "entries": {
+    "Benchmark - MessageBusIterationBenchmarks - ubuntu-latest": [
+      {
+        "commit": {
+          "author": {
+            "email": "jasonc@alertr.info",
+            "name": "Jason Couture",
+            "username": "jasoncouture"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "26ee6f96cd80e030639dbd80ce1265f550c6b935",
+          "message": "Reduce the number of benchmarks being run (#31)",
+          "timestamp": "2022-09-25T19:59:06-04:00",
+          "tree_id": "e58cb37cb9c7ae6384cbdd17512ff8513605a2bc",
+          "url": "https://github.com/jasoncouture/cachr/commit/26ee6f96cd80e030639dbd80ce1265f550c6b935"
+        },
+        "date": 1664150810052,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Cachr.Benchmarks.MessageBusIterationBenchmarks.BroadcastAsyncBenchmark",
+            "value": 9237.470533854166,
+            "unit": "ns",
+            "range": "± 1549.7416317253887"
+          },
+          {
+            "name": "Cachr.Benchmarks.MessageBusIterationBenchmarks.SendToAsyncBenchmark",
+            "value": 1102.2542106119793,
+            "unit": "ns",
+            "range": "± 38.810276179342814"
+          }
+        ]
+      }
+    ]
+  }
+}
