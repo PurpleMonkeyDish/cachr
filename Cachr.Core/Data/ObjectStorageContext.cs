6f4b4f29b60eda8a8026111d5bbcd58360ae5620
Jason Couture <jasonc@alertr.info>
2023-07-14 10:17:39 -0700

    Prepare DB Context for mapping

--- Cachr.Core/Data/ObjectStorageContext.cs
+++ Cachr.Core/Data/ObjectStorageContext.cs
@@ -1,29 +1,48 @@
 using System.ComponentModel.DataAnnotations.Schema;
-using Cachr.Core.Data.Storage;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cachr.Core.Data;
 
-public class StoredObject
+public class StorageObjectConfiguration
+{
+    public string ConnectionString { get; set; } = "Data Source=./objects.db";
+}
+
+public record CacheEntryData
+{
+    public string Key { get; init; }
+    public Guid MetadataId { get; init; }
+    public DateTimeOffset Created { get; init; }
+    public DateTimeOffset LastModified { get; init; }
+    public DateTimeOffset? AbsoluteExpiration { get; init; }
+    public TimeSpan? SlidingExpiration { get; init; }
+}
+
+internal class StoredObject
 {
     public required string Key { get; set; }
     public Guid MetadataId { get; set; }
     public long Created { get; set; } = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+    [Column("Modified")]
     public long LastUpdate { get; set; } = DateTimeOffset.Now.ToUnixTimeMilliseconds();
 }
-public class ObjectMetadata
+
+internal class StoredObjectMetadata
 {
     public Guid Id { get; init; } = Guid.NewGuid();
     public long? AbsoluteExpiration { get; init; }
     public long? SlidingExpiration { get; init; }
     public long? CurrentExpiration { get; init; }
+    public DateTimeOffset Created { get; init; }
+    public DateTimeOffset Modified { get; init; }
 }
 
 public class ObjectStorageContext : DbContext
 {
-    public static string ConnectionString { get; set; } = "Data Source=./data.db";
-
-    public ObjectStorageContext(DbContextOptions<ObjectStorageContext> options) : base(options) { }
+    public ObjectStorageContext(DbContextOptions<ObjectStorageContext> options)
+        : base(options)
+    {
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -31,13 +50,13 @@ public class ObjectStorageContext : DbContext
         modelBuilder.Entity<StoredObject>().HasKey(i => i.Key);
         modelBuilder.Entity<StoredObject>().HasIndex(i => i.MetadataId).IsUnique();
         modelBuilder.Entity<StoredObject>().Property(i => i.MetadataId).IsRequired();
-        modelBuilder.Entity<StoredObject>().HasOne<ObjectMetadata>();
+        modelBuilder.Entity<StoredObject>().HasOne<StoredObjectMetadata>();
 
-        modelBuilder.Entity<ObjectMetadata>().HasKey(i => i.Id);
-        modelBuilder.Entity<ObjectMetadata>().Property(i => i.Id).IsRequired().ValueGeneratedNever();
+        modelBuilder.Entity<StoredObjectMetadata>().HasKey(i => i.Id);
+        modelBuilder.Entity<StoredObjectMetadata>().Property(i => i.Id).IsRequired().ValueGeneratedNever();
         base.OnModelCreating(modelBuilder);
     }
 
-    public DbSet<StoredObject> StoredObjects { get; set; }
-    public DbSet<ObjectMetadata> ObjectMetadata { get; set; }
+    internal DbSet<StoredObject> StoredObjects { get; set; }
+    internal DbSet<StoredObjectMetadata> ObjectMetadata { get; set; }
 }
--- Cachr.Core/Data/Storage/StorageConfiguration.cs
+++
@@ -1,7 +0,0 @@
-namespace Cachr.Core.Data.Storage;
-
-public class StorageConfiguration
-{
-    public string Path { get; set; } = "./data/";
-    public string ConnectionString { get; set; } = "./index.db";
-}
