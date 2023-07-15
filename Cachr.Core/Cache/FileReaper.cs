ff3226e88fdd0db033fc90ce1cf60fe2cd2f7cf3
Jason Couture <jasonc@alertr.info>
2023-07-15 18:17:05 -0400

    Remove test code

--- Cachr.Core/Cache/FileReaper.cs
+++ Cachr.Core/Cache/FileReaper.cs
@@ -24,8 +24,6 @@ public class FileReaper : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        await Task.Yield();
-        await DoSomeBullshitRealQuickAsync(stoppingToken);
         using var periodicTimer = new PeriodicTimer(_options.Value.ReapInterval);
         try
         {
@@ -48,29 +46,5 @@ public class FileReaper : BackgroundService
             // This will only happen if we fail to enumerate our storage directory.
             _logger.LogCritical(ex, "FATAL: Reaper process failed due to an exception");
         }
-    }
-
-    private async Task DoSomeBullshitRealQuickAsync(CancellationToken stoppingToken)
-    {
-        await Parallel.ForEachAsync(Enumerable.Range(0, 100),
-            new ParallelOptions()
-            {
-                CancellationToken = stoppingToken, MaxDegreeOfParallelism = (Environment.ProcessorCount / 2) + 1
-            },
-            async (_, token) =>
-            {
-                await using var scope = _scopeFactory.CreateAsyncScope();
-                // Injects scoped db context, so we need to resolve it. May as well use a scope per pass.
-                var cacheStorage = scope.ServiceProvider.GetRequiredService<ICacheStorage>();
-                var shardSelector = scope.ServiceProvider.GetRequiredService<IShardSelector>();
-                var key = Guid.NewGuid().ToString("n");
-                var shard = shardSelector.SelectShard(key);
-                await cacheStorage.CreateOrReplaceEntryAsync(key,
-                    shard,
-                    DateTimeOffset.Now.AddSeconds(Random.Shared.Next(30, 600)),
-                    null,
-                    _ => Task.CompletedTask,
-                    token).ConfigureAwait(false);
-            }).ConfigureAwait(false);
     }
 }
